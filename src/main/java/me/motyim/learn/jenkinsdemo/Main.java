1114d53dc6a00d842fcc1549c76ce93db434f64e
HEADQ\Mohamed_ElMotyim <mohame.motyim@gmail.com>
2018-07-25 14:51:30 +0200

    add main class

---
+++ src/main/java/me/motyim/learn/jenkinsdemo/Main.java
@@ -0,0 +1,12 @@
+package me.motyim.learn.jenkinsdemo;
+
+/**
+ * @author MotYim <mohamed.motyim@gmail.com>
+ * @since 25-7-2018
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome in Jenkins Demo");
+    }
+
+}
